f010b152984c591dcd89423927cda67b491c2419
Raeleen <raeleen@wearehma.com>
2020-01-17 15:32:07 -0800

    first commit

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,14 @@
+export class Ingredient {
+  // public name: string;
+  // pubilc amount: number;
+
+  // constructor(name: string, amount: number){
+  //   this.name = name;
+  //   this.amount = amount;
+  // }
+
+  //SHORT HAND FOR ABOVE!
+  constructor(public name: string, public amount: number) {
+
+  }
+}
